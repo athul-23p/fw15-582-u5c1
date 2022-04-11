8ecc235f79bb665140da72f1d9ea768e85815c8a
athul23p <athul238p@gmail.com>
2022-04-11 12:13:06 +0530

    all controls  are now working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import {useState} from 'react';
+import Status from './components/Status';
+// import './App.css';
 function App() {
+
+  const [score,setScore] = useState(76);
+  const [wickets,setWicket] = useState(2);
+  const [balls,setBalls] = useState(50);
+  const handleScoreChange = (val) => {
+    if(score>100){
+      return;
+    }
+    setScore(score+val);
+  }
+  const handleWicketChange = () => {
+    if(wickets+1 === 13 || score > 100){
+      return;
+    }
+    setWicket(wickets+1);
+  }
+
+  const handleBallsChange = () => {
+    if(score > 100){
+      return ;
+
+    }
+    setBalls(balls + 1)
+  }
   return (
     <div className="App">
       <h3>India:</h3>
@@ -8,6 +35,7 @@ function App() {
           <h1 className="scoreCount">
             {
               // show "score" here
+              score
             }
           </h1>
         </div>
@@ -16,6 +44,7 @@ function App() {
           <h1 className="wicketCount">
             {
               // show wicket here
+              wickets
             }
           </h1>
         </div>
@@ -27,6 +56,7 @@ function App() {
               // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
               // if 1 more ball is thrown then over is now 5.0
               // you have to write logic to form this string from current ball number.
+              `${Math.floor(balls / 6)}.${balls % 6}`
             }
           </h1>
         </div>
@@ -35,24 +65,31 @@ function App() {
       <div className="addScore">
         Add Score
         {/* these buttons should add the respective amount in the score */}
-        <button className="addScore1">Add 1</button>
-        <button className="addScore4">Add 4</button>
-        <button className="addScore6">Add 6</button>
+        <button className="addScore1" onClick={() => handleScoreChange(1)}>
+          Add 1
+        </button>
+        <button className="addScore4" onClick={() => handleScoreChange(4)}>
+          Add 4
+        </button>
+        <button className="addScore6" onClick={() => handleScoreChange(6)}>
+          Add 6
+        </button>
       </div>
 
       <div className="addWicket">
         Add Wicket
         {/* Increase the count of wicket */}
-        <button>Add 1 wicket</button>
+        <button onClick={handleWicketChange}>Add 1 wicket</button>
       </div>
 
       <div className="addBall">
         Add ball
         {/* Increase the total number of balls thrown here. */}
-        <button>Add 1</button>
+        <button onClick={handleBallsChange}>Add 1</button>
       </div>
 
       {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+      {score > 100 && <Status />}
     </div>
   );
 }
---
+++ src/components/Status.jsx
@@ -0,0 +1,8 @@
+function Status(props){
+
+    return (<>
+        <h1 className="status">India Won</h1>
+    </>)
+}
+
+export default Status;
